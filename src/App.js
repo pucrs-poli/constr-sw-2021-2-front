bb12e4421c41b76e38a721d9ded09cb1d4550feb
bvinicius <vinicius.bazanella1@gmail.com>
2021-11-24 23:18:42 -0300

    Atualiza boilerplate e readme

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
       <Routes>
-          <Route exact path='/'   component={'telaInicial'} />
-          <Route path='/exemplo1' component={'tela1'} />
-          <Route path='/exemplo2' component={'tela2'} />
+        <Route exact path='/' element={<Home />} />
+        <Route path='/exemplo1' element={'tela1'} />
+        <Route path='/exemplo2' element={'tela2'} />
       </Routes>
     </Router>
   );
