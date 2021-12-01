37cfb51af00ce48ebb87af0dcf4c15ea23aa236f
Matheu Góes <44243453+matheugoes@users.noreply.github.com>
2021-12-01 00:22:18 -0300

    Modal create

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -1,44 +1,47 @@
-import { MenuBook, Search, Add } from "@mui/icons-material";
-import { Button, InputAdornment, InputBase } from "@mui/material";
-import { Box, width } from "@mui/system";
+import { Person } from "@mui/icons-material";
+import { InputBase } from "@mui/material";
+import { Box } from "@mui/system";
 import UserTable from '../../components/Users/UserTable';
+import UserModal from '../../components/Users/UserModal';
 
 import './Users.css';
 
-export default function Classes() {
+export default function Users() {
 
-    const mockClass = { name: 'Gabriel Rabelo', reg: '123456789', email: 'rabelo@example.com', roles: ['admin', 'professor'] };
-    const mockClasses = Array(4).fill(mockClass);
+    const mockUser = { name: 'Gabriel Rabelo', reg: '123456789', email: 'rabelo@example.com', roles: ['admin', 'professor'] };
+    const mockUsers = Array(10).fill(mockUser);
 
     return (
-        <Box sx={{mt: 4}} class="users-content">
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
-                <div class="title">
-                    <MenuBook fontSize="large" style={{fill: "#647A79"}}/>
-                    <Box sx={{ ml: 1 }}>
-                        Usuários
+        <div>
+            <Box sx={{mt: 4}} class="users-content">
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
+                    <div class="title">
+                        <Person fontSize="large" style={{fill: "#647A79"}}/>
+                        <Box sx={{ ml: 1 }}>
+                            Usuários
+                        </Box>
+                    </div>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center'}}>
+                        <InputBase
+                            sx={{ mr: 1 }}
+                            placeholder="Pesquisar"
+                            inputProps={{
+                                style:{
+                                    backgroundColor: '#F2F2F2',
+                                    color: "#647A79",
+                                    borderRadius: '6px',
+                                    padding: '7px 16px',
+                                    width: '150px',
+                                }
+                            }}
+                        />
+                        <UserModal/>
                     </Box>
-                </div>
-
-                <Box sx={{ display: 'flex', alignItems: 'center'}}>
-                    <InputBase
-                        sx={{ mr: 1 }}
-                        placeholder="Pesquisar"
-                        inputProps={{
-                            style:{
-                                backgroundColor: '#F2F2F2',
-                                color: "#647A79",
-                                borderRadius: '6px',
-                                padding: '7px 16px',
-                                width: '120px',
-                            }
-                        }}
-                    />
-                    <Button variant="contained" startIcon={<Add/>}>Adicionar</Button>
                 </Box>
-            </Box>
 
-            <UserTable items={mockClasses}></UserTable>
-        </Box >
+                <UserTable items={mockUsers}></UserTable>
+            </Box >
+        </div>
     );
 }
